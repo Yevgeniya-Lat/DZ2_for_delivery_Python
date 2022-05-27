c6a2e969f159e3c674085f89e9a6e507fa3ed8e0
Yevgeniya-Lat <e9618281262@gmail.com>
2022-05-27 11:03:36 +0300

    Second commit

--- DZ_Seminar_2.py
+++ DZ_Seminar_2.py
@@ -1,127 +1,106 @@
-#  1. ++++++++
-print('Задача 1 => Найти сумму чисел списка стоящих на нечетной позиции')
+# #  1. ++++++++
+# print('Задача 1 => Найти сумму чисел списка стоящих на нечетной позиции')
 
-print ('Пример:  [1,2,3,4] -> 6')
+# print ('Пример:  [1,2,3,4] -> 6')
 
-print ('Пример:  [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12')
-print()
-print ('ПЕРВЫЙ ВАРИАНТ') 
-rosters =  list(map(int, input("Введите список состоящий из чисел через пробел:\n").split()))
+# print ('Пример:  [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12')
+# print()
+# print ('ПЕРВЫЙ ВАРИАНТ') 
+# rosters =  list(map(int, input("Введите список состоящий из чисел через пробел:\n").split()))
 
-def Sum_Index(l1st):
-    s = 0
-    for i in range(len(l1st)):
-        if i % 2 != 0:
-            s += l1st[i]
-    return s
+# def Sum_Index(l1st):
+#     s = 0
+#     for i in range(len(l1st)):
+#         if i % 2 != 0:
+#             s += l1st[i]
+#     return s
 
-print(f'Сумма чисел списка стоящих на нечетных позициях = {Sum_Index(rosters)}')
-print()
-print ('ВТОРОЙ ВАРИАНТ') 
+# print(f'Сумма чисел списка стоящих на нечетных позициях = {Sum_Index(rosters)}')
+# print()
+# print ('ВТОРОЙ ВАРИАНТ') 
 
-A =  int(input("Введите число A для создания списка (от A до N):\n"))
-N =  int(input("Введите число N для создания списка (от A до N):\n"))
-B =  int(input("Введите число шага в списке (от A до N):\n"))
+# A =  int(input("Введите число A для создания списка (от A до N):\n"))
+# N =  int(input("Введите число N для создания списка (от A до N):\n"))
+# B =  int(input("Введите число шага в списке (от A до N):\n"))
 
-rosters = list(range(A, N+1, B))
+# rosters = list(range(A, N+1, B))
 
-print(rosters) 
-print()
-def sum_odd_position(mylist):
-    return sum(mylist[1::2])
+# print(rosters) 
+# print()
+# def sum_odd_position(mylist):
+#     return sum(mylist[1::2])
 
-print(f'Сумма чисел списка стоящих на нечетных позициях = {sum_odd_position(rosters)}') 
-print()
-# # #================================ Для справки ================
-# # text = '0123456789'
-# # print(text[1::2]) # 13579
-# # print(text[2::2]) # 2468  # ==================
-# # ====================================================
+# print(f'Сумма чисел списка стоящих на нечетных позициях = {sum_odd_position(rosters)}') 
+# print()
+# # # #================================ Для справки ================
+# # # text = '0123456789'
+# # # print(text[1::2]) # 13579
+# # # print(text[2::2]) # 2468  # ==================
+# # # ====================================================
 
-# 2. ++++++++
-print('Задача 2 => Найти произведение пар чисел в списке. ') 
-print('Парой считаем первый и последний элемент, второй и предпоследний и т.д.') 
+# # 2. ++++++++
+# print('Задача 2 => Найти произведение пар чисел в списке. ') 
+# print('Парой считаем первый и последний элемент, второй и предпоследний и т.д.') 
 
-print('Пример: [2, 3, 4, 5, 6] => [12, 15, 16]; ') 
-print(' [2, 3, 5, 6] => [12, 15] ')      
-print()
-print('Первый способ: ')
+# print('Пример: [2, 3, 4, 5, 6] => [12, 15, 16]; ') 
+# print(' [2, 3, 5, 6] => [12, 15] ')      
+# print()
+# print('Первый способ: ')
 
-def Product_Pairs_Numbers(L1st): 
-    new_L1st = len(L1st)//2 + 1 if len(L1st) % 2 != 0 else len(L1st)//2
-    product_L1st = [L1st[i]*L1st[len(L1st)-i-1] for i in range(new_L1st)]
-    return product_L1st
+# def Product_Pairs_Numbers(L1st): 
+#     new_L1st = len(L1st)//2 + 1 if len(L1st) % 2 != 0 else len(L1st)//2
+#     product_L1st = [L1st[i]*L1st[len(L1st)-i-1] for i in range(new_L1st)]
+#     return product_L1st
 
-rosters1 = list(map(int, input("Введите числа через пробел:\n").split()))
-print (f'Произведение пар чисел в списке равно => {Product_Pairs_Numbers(rosters1)}')
-print()
-# # ========================= 
+# rosters1 = list(map(int, input("Введите числа через пробел:\n").split()))
+# print (f'Произведение пар чисел в списке равно => {Product_Pairs_Numbers(rosters1)}')
+# print()
+# # # ========================= 
 
-print('Второй способ:') 
+# print('Второй способ:') 
 
-def Product_Pairs_Numbers2(L1st):
-    result = []
-    while len(L1st) > 1:
-        result.append(L1st[0]*L1st[-1])
-        del L1st[0] 
-        del L1st[-1] 
-    if len(L1st) ==1: result.append(L1st[0]**2)       
-    return result
+# def Product_Pairs_Numbers2(L1st):
+#     result = []
+#     while len(L1st) > 1:
+#         result.append(L1st[0]*L1st[-1])
+#         del L1st[0] 
+#         del L1st[-1] 
+#     if len(L1st) ==1: result.append(L1st[0]**2)       
+#     return result
 
-rosters2 = list(map(int, input("Введите числа через пробел:\n").split()))
+# rosters2 = list(map(int, input("Введите числа через пробел:\n").split()))
 
-print(f'Произведение пар чисел в списке равно => {Product_Pairs_Numbers2(rosters2)}')               
-print()
-# ===================================================
+# print(f'Произведение пар чисел в списке равно => {Product_Pairs_Numbers2(rosters2)}')               
+# print()
+# # ===================================================
 
 # 3. 
+
 print('Задача 3 => В заданном списке вещественных чисел найдите разницу ') 
 print('между максимальным и минимальным значением дробной части элементов.') 
 
 print('Пример: [1.1, 1.2, 3.1, 5, 10.01] => 0.19')
 print()
 
-rosters3 = list(map(float, input("Введите вещественные числа через пробел:\n").split()))
-print(rosters3)
+rosters3 = list(map(float, input("Введите числа через пробел:\n").split()))
+# print(rosters3)
 
-print(f'Разница между максимальным и минимальным значением дробной части элементов равно => {max(rosters3) - min(rosters3)}')
+def funcial(my_list):
+    a = []
+    for i in my_list:
+        if i%1 != 0:
+            j = round(i%1,2)
+            a.append(j)
+    return a
 
+# print(funcial(rosters3))
 
-# my_rosters = [round(i%1,2) for i in rosters3 if i%1 != 0]
-# print(max(my_rosters) - min(my_rosters))
+my_list1 = funcial(rosters3)
+# print(min(my_list1))
+# print(max(my_list1))
 
+print(f'Разница между максимальным и минимальным значением дробной части элементов равно => {max(my_list1) - min(my_list1)}')
 
-# # =================================  
-# from random import randint
-# import math
-
-# def get_numbers(n, frst, last):
-#     return [randint(frst, last) for i in range(n)]
-
-# def mult_pairs(mylist):
-#     return [mylist[i] * mylist[-i - 1] for i in range(math.ceil(len(mylist)/2))]
-
-# n = 9
-# frst = 1
-# last = 10
-
-# mylist = get_numbers(n, frst, last)
-# print(mylist)
-# print(mult_pairs(mylist))
-
-
-# # Второй способ:
-
-# def pairs_mult(numbers):
-#     results = []
-#     while len(numbers) > 1:
-#         results.append(numbers[0]*numbers[-1])
-#         del numbers[0] 
-#         del numbers[-1] 
-#     if len(numbers) ==1: results.append(numbers[0]**2)       
-#     return results
-
-# print(pairs_mult(mylist))          # ========================
 
 # # ======================================================
 
